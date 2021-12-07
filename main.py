9dfd8f945e80bacaa659dce0a9b9e65e22dfb1eb
michael mcarthur <mmcarthur@spartaglobal.com>
2021-12-07 10:49:35 +0000

    Added Jupyter visualisation files and csv data sets

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+
+sns.set_theme()
+
+GOT = pd.read_csv('GOT_episodes_v4.csv')
+print(GOT)
